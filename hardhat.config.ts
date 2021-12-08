b9a183d4120095615098df55d9a664df01c57664
zeeshanhanif <zeeshanhanif@gmail.com>
2021-12-08 20:25:02 +0500

    Added dotenv module and added network details for deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,16 @@ import "@nomiclabs/hardhat-waffle";
 import '@typechain/hardhat';
 import '@nomiclabs/hardhat-ethers';
 import 'hardhat-contract-sizer';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const INFURA_KEY = process.env.INFURA_KEY;
+// Replace this private key with your account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -22,10 +32,41 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 export default {
   solidity: "0.8.4",
+  settings: {
+    // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+    optimizer: {
+      enabled: true,
+      runs: 1000000,
+    },
+  },
+  paths: {
+    artifacts: "build/artifacts",
+    cache: "build/cache",
+    deploy: "src/deploy",
+    sources: "contracts",
+  },
   contractSizer: {
     alphaSort: true,
     disambiguatePaths: false,
     runOnCompile: true,
     strict: true,
-  }
+  },
+  networks: {
+    localhost: {
+      url:' http://127.0.0.1:8545/'
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+    /*
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${INFURA_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${INFURA_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }*/
+  },
 };
