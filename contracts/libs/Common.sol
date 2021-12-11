04e0a0e8243af5e2a3c33488c5e22b469465c22c
zeeshanhanif <zeeshanhanif@gmail.com>
2021-12-12 01:27:17 +0500

    Added common library for structs

---
+++ contracts/libs/Common.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Common {
+
+    struct APIDAOConfig {
+        string apiProposalId;
+        string apiId;
+        string apiTitle;
+        string apiType;
+        string daoName;
+        uint256 votingSupportPercentage;
+        uint256 votingMinimumApprovalPercentage;
+        uint256 voteDuration;
+    }
+
+    struct APITokenConfig {
+        string apiTokenName;
+        string apiTokenSymbol;
+        uint256 maxApiTokenSupply;
+        uint256 initialApiTokenSupply;
+        uint256 developerSharePercentage;
+        uint256 apiInvestorSharePercentage;
+        uint256 thresholdForSubscriberMinting;
+    }
+}
