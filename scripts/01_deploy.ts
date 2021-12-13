4750cfbb50007e50a3978e47d285760f604a3ee0
zeeshanhanif <zeeshanhanif@gmail.com>
2021-12-13 22:34:20 +0500

    Added deployment script to include all the contracts deployment and initialization

--- scripts/01_deploy.ts
+++ scripts/01_deploy.ts
@@ -26,21 +26,23 @@ async function main() {
   const PanaCoin:PanaCoin__factory = await ethers.getContractFactory("PanaCoin");
   const panaCoin:PanaCoin = await PanaCoin.deploy();
   await panaCoin.deployed();
-  console.log("panaCoin deployed to:", panaCoin.address);
+  console.log("PanaCoin deployed to:", panaCoin.address);
 
   const APINFT:APINFT__factory = await ethers.getContractFactory("APINFT");
   const apiNFT:APINFT = await APINFT.deploy();
   await apiNFT.deployed();
   console.log("APINFT deployed to:", apiNFT.address);
 
+
   const PanacloudPlatform:PanacloudPlatform__factory = await ethers.getContractFactory("PanacloudPlatform");
   const panacloudPlatform:PanacloudPlatform = await PanacloudPlatform.deploy();
   await panacloudPlatform.deployed();
   console.log("PanacloudPlatform deployed to:", panacloudPlatform.address);
   const txt1:ContractTransaction = await panacloudPlatform.initialize(paymentSplitter.address);
-  console.log("panacloudPlatform.initialize transaction:", txt1.hash);
+  //const txt1:ContractTransaction = await panacloudPlatform.initialize("0xd88Ab12f329C3e5dBaD1092b7E4710a7C3B2731C");
+  console.log("PanacloudPlatform.initialize transaction hash:", txt1.hash);
   const txtReceipt1:ContractReceipt = await txt1.wait();
-  console.log("panacloudPlatform.initialize transaction completed");
+  console.log("PanacloudPlatform.initialize transaction completed");
 
 
   const PanaFactory:PanaFactory__factory = await ethers.getContractFactory("PanaFactory");
@@ -48,7 +50,8 @@ async function main() {
   await panaFactory.deployed();
   console.log("PanaFactory deployed to:", panaFactory.address);
   const txt2:ContractTransaction = await panaFactory.initialize(panaCoin.address, apiNFT.address, panacloudPlatform.address, apiTokenFactory.address, daoFactory.address);
-  console.log("panaFactory.initialize transaction:", txt2.hash);
+  //const txt2:ContractTransaction = await panaFactory.initialize("0xFda643aE677a2155795a9e3154C691d9Df18237d", "0x6825755326cF5cc3BB432b0A05060f3dA2D7eCa7", panacloudPlatform.address, "0x18630eC22859f7E7725513b1CEcCa7df2dB764B6", "0x4E9b5f6B43d5A497BB3Eb45a1474694Da1761Ac2");
+  console.log("panaFactory.initialize transaction hash:", txt2.hash);
   const txtReceipt2:ContractReceipt = await txt2.wait();
   console.log("panaFactory.initialize transaction completed");
 
@@ -64,11 +67,14 @@ async function main() {
   console.log("PlatformGoverner deployed to:", platformGoverner.address);
   
   const txt3:ContractTransaction = await platformGoverner.initialize(panaFactory.address,timelock.address, panaCoin.address);
-  console.log("PlatformGoverner.initialize transaction:", txt3.hash);
+  //const txt3:ContractTransaction = await platformGoverner.initialize("0xC58B924Feb50C42f4F6226D50c3f53f11ACbD536","0xA6854e9Df13cCCD78Ad53EF249B2eA0Eb2E7bF6c", "0xFda643aE677a2155795a9e3154C691d9Df18237d");
+  console.log("PlatformGoverner.initialize transaction hash:", txt3.hash);
   const txtReceipt3:ContractReceipt = await txt3.wait();
   console.log("PlatformGoverner.initialize transaction completed");
   
 
+  /*
+  // Not needed for now
   const PanacloudController:PanacloudController__factory = await ethers.getContractFactory("PanacloudController");
   const panacloudController:PanacloudController = await PanacloudController.deploy();
   await panacloudController.deployed();
@@ -78,6 +84,7 @@ async function main() {
   console.log("PanacloudController.initialize transaction:", txt4.hash);
   const txtReceipt4:ContractReceipt = await txt4.wait();
   console.log("PanacloudController.initialize transaction completed");
+  */
 
 }
 
