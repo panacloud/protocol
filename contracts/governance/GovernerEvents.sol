5aaa99517d13794e1702e5afb580777ef17984e7
zeeshanhanif <zeeshanhanif@gmail.com>
2021-12-29 00:28:17 +0500

    Updated the GovernorEvents

--- contracts/governance/GovernerEvents.sol
+++ contracts/governance/GovernerEvents.sol
@@ -3,18 +3,47 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract GovernerEvents {
 
-    event ProposalCreated(uint id, address proposer, string apiTitle, string[] highLevelFeatures, string documentationURL, string description, uint startBlock, uint endBlock);
+    /// @notice An event emitted when a new proposal is created
+    event ProposalCreated(uint id, address proposer, address[] targets, uint[] values, string[] signatures, bytes[] calldatas, uint startBlock, uint endBlock, string description);
 
+    /// @notice An event emitted when a vote has been cast on a proposal
+    /// @param voter The address which casted a vote
+    /// @param proposalId The proposal id which was voted on
     /// @param support Support value for the vote. 0=against, 1=for, 2=abstain
+    /// @param votes Number of votes which were cast by the voter
+    /// @param reason The reason given for the vote by the voter
     event VoteCast(address indexed voter, uint proposalId, uint8 support, uint votes, string reason);
 
+    /// @notice An event emitted when a proposal has been canceled
     event ProposalCanceled(uint id);
+
+    /// @notice An event emitted when a proposal has been queued in the Timelock
     event ProposalQueued(uint id, uint eta);
+
+    /// @notice An event emitted when a proposal has been executed in the Timelock
     event ProposalExecuted(uint id);
+
+    /// @notice An event emitted when the voting delay is set
     event VotingDelaySet(uint oldVotingDelay, uint newVotingDelay);
+
+    /// @notice An event emitted when the voting period is set
     event VotingPeriodSet(uint oldVotingPeriod, uint newVotingPeriod);
+
+    /// @notice Emitted when implementation is changed
+    event NewImplementation(address oldImplementation, address newImplementation);
+
+    /// @notice Emitted when proposal threshold is set
     event ProposalThresholdSet(uint oldProposalThreshold, uint newProposalThreshold);
 
+    /// @notice Emitted when pendingAdmin is changed
     event NewPendingAdmin(address oldPendingAdmin, address newPendingAdmin);
+
+    /// @notice Emitted when pendingAdmin is accepted, which means admin is updated
     event NewAdmin(address oldAdmin, address newAdmin);
+
+    /// @notice Emitted when whitelist account expiration is set
+    event WhitelistAccountExpirationSet(address account, uint expiration);
+
+    /// @notice Emitted when the whitelistGuardian is set
+    event WhitelistGuardianSet(address oldGuardian, address newGuardian);
 }
