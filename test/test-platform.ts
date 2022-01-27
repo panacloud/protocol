155105893c5d00e5b778f31c4822de502af368e3
zeeshanhanif <zeeshanhanif@gmail.com>
2022-01-28 00:22:33 +0500

    Added DAI token in testcases for fund transfer

--- test/test-platform.ts
+++ test/test-platform.ts
@@ -1,23 +1,39 @@
 import { ethers, waffle } from "hardhat";
 import { expect } from "chai";
 import { Address } from "cluster";
-import { APIToken, APIToken__factory, PanacloudPlatform, PanacloudPlatform__factory } from "../typechain";
+import { APIToken, APIToken__factory, DAIToken, DAIToken__factory, PanacloudPlatform, PanacloudPlatform__factory } from "../typechain";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { BigNumber } from "@ethersproject/bignumber";
 
+// DAI rinkeby address
+// 0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea
+
+// DAI Mainnet address
+// 0x6b175474e89094c44da98b954eedeac495271d0f
 
 describe("Panacloud Platform Test", function () {
     
     let panacloudPlatform:PanacloudPlatform;
+    let daiToken:DAIToken;
     let apiTokenAddress:string;
     let apiDAOAddress:string;
     let apiId:string;
 
     before(async()=>{
-        const [owner, addr1, addr2,addr3,addr4,addr5]: SignerWithAddress[] = await ethers.getSigners();
+        const [owner, addr1, addr2, addr3, addr4, addr5]: SignerWithAddress[] = await ethers.getSigners();
+        
+        const DAIToken:DAIToken__factory = await ethers.getContractFactory("DAIToken");
+        daiToken = await DAIToken.deploy();
+        await daiToken.deployed();
+        
         const PanacloudPlatform:PanacloudPlatform__factory = await ethers.getContractFactory("PanacloudPlatform");
         panacloudPlatform = await PanacloudPlatform.deploy();
         await panacloudPlatform.deployed();
+
+        await panacloudPlatform.initialize(addr5.address,daiToken.address,owner.address);
+
+        await daiToken.mint(addr1.address,10000);
+
         // This is just for dummy testing, in actual we need contract addresses
         apiTokenAddress = addr3.address;
         apiDAOAddress = addr4.address;
@@ -26,14 +42,18 @@ describe("Panacloud Platform Test", function () {
 
     it("API DAO should be created successfully", async function () {
         const [owner, addr1, addr2]: SignerWithAddress[] = await ethers.getSigners();
-        
         expect(await panacloudPlatform.connect(owner).apiDAOCreated(owner.address,apiId,apiTokenAddress,apiDAOAddress)).to.be.ok;
-
     });
 
 
     it("2 Invoice Payment Made successfully", async function () {
         const [owner, addr1, addr2]: SignerWithAddress[] = await ethers.getSigners();
+
+        const addr1DaiBalance = await daiToken.balanceOf(addr1.address);
+        //console.log("addr1DaiBalance = ",addr1DaiBalance.toNumber());
+
+        expect(await daiToken.connect(addr1).approve(panacloudPlatform.address,400)).to.be.ok;
+
         expect(await panacloudPlatform.connect(addr1).payInvoice(owner.address, apiDAOAddress,{
             apiToken: apiTokenAddress,
             dueDate: Date.now(),
@@ -57,8 +77,8 @@ describe("Panacloud Platform Test", function () {
         const [owner, addr1]: SignerWithAddress[] = await ethers.getSigners();
         const devEarningDetails = await panacloudPlatform.getDevEarnings(owner.address);
         //console.log("Earning details = ",devEarningDetails);
-        expect(devEarningDetails[0].toNumber()).to.be.equal(400);
-        expect(devEarningDetails[1].toNumber()).to.be.equal(400);
+        expect(devEarningDetails[0].toNumber()).to.be.equal(380);
+        expect(devEarningDetails[1].toNumber()).to.be.equal(380);
         expect(devEarningDetails[2].toNumber()).to.be.equal(0);
         expect(devEarningDetails[3][0].apiDao).to.be.equal(apiDAOAddress);
         expect(devEarningDetails[3][0].apiToken).to.be.equal(apiTokenAddress);
