2c7b7989780c432970f39bfc81b8c8dc6bafb175
zeeshanhanif <zeeshanhanif@gmail.com>
2022-08-25 20:02:15 +0500

    Added white listing criteria start date and end date. Also added events to apply in investment pool

--- contracts/investments/InvestmentPools.sol
+++ contracts/investments/InvestmentPools.sol
@@ -19,11 +19,20 @@ contract InvestmentPools is Ownable  {
     mapping(address => Global.PoolInfo) public apiInvestmentPool;
     // key: User wallet address, value: Array of PoolInfo
     mapping(address => Global.PoolInfo[]) public userInvestmentPools;
+    // key: API Token address, value: WhitelistCriteria
+    mapping(address => Global.WhitelistCriteria) public apiWhitelistCriteria;
+    
     // List of All Pools
     Global.PoolInfo[] public poolList;
 
+    // Key: API Token Address, Value: Mapping= Key: User Address, value: true/false
+    mapping(address => mapping(address => bool)) allowedList;
+
     uint256 private totalFundsApproved;
     uint256 private totalFundsAvailable;
+
+    event InvestmentPoolCreated(address apiDev, address apiToken, uint256 poolIndex);
+    event PaymentMilestoneClaimCreated(address apiToken, uint256 amountToBeReleased);
 
     constructor() {
     }
@@ -38,7 +47,8 @@ contract InvestmentPools is Ownable  {
 
     function createInvestmentPool(address apiDev, address apiToken, uint256 startDate, 
                                 uint256 duration, uint256 tokenPrice, uint256 tokensToBeIssued, 
-                                uint256 minimumInvestmentRequired, uint256 tokenPerInvestor) 
+                                uint256 minimumInvestmentRequired, uint256 tokenPerInvestor,
+                                uint256 whitelistingStartDate, uint256 whitelistingEndDate) 
                                 public  {
         require(apiToken != address(0), "NULL API Token Address Provided");
         Global.PoolInfo storage _poolInfo = apiInvestmentPool[apiToken];
@@ -57,7 +67,10 @@ contract InvestmentPools is Ownable  {
 
         userInvestmentPools[apiDev].push(_poolInfo);
 
+        apiWhitelistCriteria[apiToken] = Global.WhitelistCriteria(poolCounter,apiToken,whitelistingStartDate,whitelistingEndDate);
+
         poolList.push(_poolInfo);
+        emit InvestmentPoolCreated(apiDev, apiToken, poolCounter);
         poolCounter++;
     }
 
@@ -69,6 +82,14 @@ contract InvestmentPools is Ownable  {
 
     function getInvestmentPool(address _apiToken) public view returns (Global.PoolInfo memory) {
         return apiInvestmentPool[_apiToken];
+    }
+
+    function getWhitelistCriteria(address _apiToken) public view returns (Global.WhitelistCriteria memory) {
+        return apiWhitelistCriteria[_apiToken];
+    }
+
+    function getPoolInfoList() public view returns (Global.PoolInfo[] memory) {
+        return poolList;
     }
 
 }
--- contracts/investments/InvestmentPoolsManager.sol
+++ contracts/investments/InvestmentPoolsManager.sol
@@ -5,6 +5,7 @@ import "./InvestmentPools.sol";
 //import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../governance/PanaCoin.sol";
 import "../libs/Global.sol";
+import "hardhat/console.sol";
 
 // Central Point for all the contract to initialize
 contract InvestmentPoolsManager is Ownable  {
@@ -17,18 +18,34 @@ contract InvestmentPoolsManager is Ownable  {
     mapping(address => mapping(address=>bool)) whitelisters;
     //mapping(address => )
 
+    event AppliedForInvestmentPool(address apiToken, address userAddress);
+    event InvestedInPool(address apiToken, uint256 investmentAmount, address userAddress);
+
     function initialize(address _investmentPools, address _panacoin) public onlyOwner {
         investmentPools = InvestmentPools(_investmentPools);
         panaCoin = PanaCoin(_panacoin);
     }
 
-
     function applyForInvestmentPool(address _apiToken) public {
+        console.log("Inside applyForInvestmentPool api token = ",_apiToken);
         uint256 availableBalance = panaCoin.balanceOf(msg.sender);
+        console.log("after availableBalance = ",availableBalance);
         require(availableBalance >= amountForWhitelisting, "Required Balance not available");
         Global.PoolInfo memory _poolInfo = investmentPools.getInvestmentPool(_apiToken);
+        Global.WhitelistCriteria memory _whitelistCriteria = investmentPools.getWhitelistCriteria(_apiToken);
+        console.log("Global.PoolInfo.apiToken = ",_poolInfo.apiToken);
+        console.log("Global.PoolInfo.apiDev = ",_poolInfo.apiDev);
+        console.log("Global.PoolInfo.startDate = ",_poolInfo.startDate);
+        console.log("Global.PoolInfo.tokensToBeIssued = ",_poolInfo.tokensToBeIssued);
+        
+        console.log("Global.WhitelistCriteria.whitelistingStartDate = ",_whitelistCriteria.whitelistingStartDate);
+        console.log("Global.WhitelistCriteria.whitelistingEndDate = ",_whitelistCriteria.whitelistingEndDate);
+        console.log("block.timestamp = ",block.timestamp);
+
+        require(block.timestamp >= _whitelistCriteria.whitelistingStartDate && block.timestamp<= _whitelistCriteria.whitelistingEndDate, "Whitelisting start and end time not match");
         require(_poolInfo.apiToken == _apiToken, "Pool Not found");
         whitelisters[_apiToken][msg.sender] = true;
+        emit AppliedForInvestmentPool(_apiToken, msg.sender);
     }
 
     function setAmountForWhitelisting(uint256 _amountForWhitelisting) public onlyOwner {
@@ -42,6 +59,7 @@ contract InvestmentPoolsManager is Ownable  {
         //require(allowance >= (price * tokenQuantity),"Insufficient approval for funds");
         require(allowance >= _investmentAmount,"Insufficient approval for funds");
         panaCoin.transferFrom(msg.sender, address(this), _investmentAmount);
+        emit InvestedInPool(_apiToken, _investmentAmount, msg.sender);
     }
 
 
