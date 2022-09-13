c9756251a7b6c7afa0d8857d773c72b8e435cb60
zeeshanhanif <zeeshanhanif@gmail.com>
2022-09-13 21:11:40 +0500

    Added script for invest in pool

---
+++ scripts/08_invest-in-pool.ts
@@ -0,0 +1,68 @@
+import { ContractReceipt, ContractTransaction } from '@ethersproject/contracts';
+import { BigNumber } from 'ethers';
+import { ethers, run, network } from 'hardhat';
+import { InvestmentPools, InvestmentPoolsManager, InvestmentPoolsManager__factory, InvestmentPools__factory, PanacloudPlatform, PanacloudPlatform__factory, PanaCoin, PanaCoin__factory } from '../typechain';
+
+const deployedAddresses = require("../deployment/addresses.json");
+
+async function main() {
+
+  const [owner, addr1] = await ethers.getSigners();
+  const networkName = network.name; 
+  console.log("Network = ",networkName);
+  
+  const investor = addr1;
+  
+  const InvestmentPools:InvestmentPools__factory = await ethers.getContractFactory("InvestmentPools");
+  const investmentPools:InvestmentPools = await InvestmentPools.attach(deployedAddresses[networkName].investmentPools);
+  console.log("InvestmentPools Address to:", investmentPools.address);
+
+  const PanaCoin:PanaCoin__factory = await ethers.getContractFactory("PanaCoin");
+  const panaCoin:PanaCoin = await PanaCoin.attach(deployedAddresses[networkName].panaCoin);
+  console.log("PanaCoin Address to:", panaCoin.address);
+
+  const blockNumBefore = await ethers.provider.getBlockNumber();
+  const blockBefore = await ethers.provider.getBlock(blockNumBefore);
+  const timestampBefore = blockBefore.timestamp;
+  console.log("blockNumBefore = ", blockNumBefore);
+  console.log("blockBefore = ", blockBefore);
+  console.log("timestamp = ", timestampBefore);
+
+  //Address of User
+  const panacoinBalance = await panaCoin.connect(investor).balanceOf(investor.address)
+  console.log("PanaCoin Balance for signer = ", ethers.utils.formatEther(panacoinBalance));
+
+  const txtPanaCoinApproval = await panaCoin.connect(investor).approve(investmentPools.address, ethers.utils.parseEther("10"));
+  const receipt = txtPanaCoinApproval.wait();
+  console.log("Approval done");
+
+  const txtPanaCoinApprovalAmount = await panaCoin.connect(investor).allowance(investor.address, investmentPools.address);
+  console.log("txtPanaCoinApprovalAmount = ",txtPanaCoinApprovalAmount.toString() );
+
+                       
+  // API token id and investment amount
+  const txt1 = await investmentPools.connect(investor).investInPool("0x94099942864EA81cCF197E9D71ac53310b1468D8",ethers.utils.parseEther("10"));
+
+  console.log("investmentPools.investInPool Hash: ",txt1.hash);
+  const txtReceipt = await txt1.wait();
+  console.log("investmentPools.investInPool transaction receipt: ",txtReceipt);
+  
+
+  /*
+  const blockNumBefore = await ethers.provider.getBlockNumber();
+  const blockBefore = await ethers.provider.getBlock(blockNumBefore);
+  const timestampBefore = blockBefore.timestamp;
+  console.log("blockNumBefore = ", blockNumBefore);
+  console.log("blockBefore = ", blockBefore);
+  console.log("timestamp = ", timestampBefore);
+  */
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
